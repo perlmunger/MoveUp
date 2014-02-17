5fc3e7ed5aff2759e666c372ec9a676f5668d720
Matt Long <matt.long@matthew-long.com>
2014-02-17 13:21:44 -0700

    Initial Commit

---
+++ MoveUpTests/MoveUpTests.m
@@ -0,0 +1,34 @@
+//
+//  MoveUpTests.m
+//  MoveUpTests
+//
+//  Created by Matt Long on 2/17/14.
+//  Copyright (c) 2014 Matt Long. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MoveUpTests : XCTestCase
+
+@end
+
+@implementation MoveUpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
